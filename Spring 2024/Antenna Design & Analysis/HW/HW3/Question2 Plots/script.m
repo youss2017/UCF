e7141d0ce603d22449d68e099abeef07f64dd400
Youssef Samwel <samwelyoussef123.2@gmail.com>
2024-03-24 17:37:10 -0400

    3/24/2024 HW3 in progress

--- Spring 2024/Antenna Design & Analysis/HW/HW3/Question2 Plots/script.m
+++ Spring 2024/Antenna Design & Analysis/HW/HW3/Question2 Plots/script.m
@@ -1,6 +1,24 @@
-clear all;
-close all;
-clc;
+clear all
+close all
+clc
+% https://www.desmos.com/calculator/7dmxksal1e
+%% Design Parameters
+lambda = 299792458/800e6;
+C = lambda;
+alpha = 13.5;
+S = C * tan(deg2rad(alpha));
+N = 5;
+D_0 = 10*log10(15*N*(C^2*S)/(lambda^3));
+HPBW = ((52*lambda^(3/2))/(C*sqrt(N*S)));
+
+%% E-Plane Plot
+syms theta
+k_0 = (2*pi)/lambda;
+L_0 = sqrt(S^2 + C^2);
+p = (L_0/lambda)/(S/lambda + 1);
+psi = k_0 * (S * cos(theta) - L_0/p);
+
+E(theta) = sin(pi/(2*N))*cos(theta)*( (sin((N/2)*psi)) / (sin(psi/2))  );
 
 list = linspace(0.7, 1.0, 4);
 f = figure;
@@ -9,24 +27,26 @@ centerfig(f);
 
 for i=1:1:length(list)
     d = list(i);
-    kd = (2*pi)*d;
+    kd = (2*pi)*(d/2);
     %theta = 0.1;
     %phi = 0;
     
     clear AF
     syms theta phi
     
-    AF(theta, phi) = exp((1j*kd/sqrt(2))*(sin(theta)*cos(phi) + sin(theta)*sin(phi))) + ...
-        exp((1j*kd/sqrt(2))*(-sin(theta)*cos(phi) + sin(theta)*sin(phi))) + ...
-        exp((-1j*kd/sqrt(2))*(sin(theta)*cos(phi) + sin(theta)*sin(phi))) + ...
-        exp((1j*kd/sqrt(2))*(sin(theta)*cos(phi) - sin(theta)*sin(phi)));
+    AF(theta, phi) = 2*cos( (kd*sqrt(2)) * ( sin(theta)*cos(phi) + sin(theta)*sin(phi) ) ) + ...
+        2*cos( (kd*sqrt(2)) * ( -sin(theta)*cos(phi) + sin(theta)*sin(phi) ) );
     
-    theta = linspace(-pi/4, pi/4, 1000);
-    phi = 0;
+    Electric_Trans(theta) = E(theta);
+
+    theta = linspace(-pi, pi, 1000);
+    phi = pi/2;
     
-    AF_eval = eval(AF) .^ 2;
+    AF_eval = abs(eval(Electric_Trans) .^ 2);
     AF_eval = AF_eval ./ max(AF_eval);
-    polarplot(theta,10.0.*log10(abs(AF_eval)),  "DisplayName", sprintf("d=%.2f", d));
+    AF_eval = 10.0 .* log10(AF_eval);
+    AF_eval(AF_eval < -15) = -15;
+    polarplot(theta, AF_eval,  "DisplayName", sprintf("d=%.2f", d));
     hold on;
     %AF_matlab = matlabFunction(eval(-AF));
     %x = fminbnd(AF_matlab, 0.5, 1.0);
--- Spring 2024/Antenna Design & Analysis/HW/HW3/Question3 Plots/script.m
+++ Spring 2024/Antenna Design & Analysis/HW/HW3/Question3 Plots/script.m
@@ -2,4 +2,21 @@ clear all
 close all
 clc
 
-E = 
+syms theta
+
+Element_Factor(theta) = cos(theta);
+Array_Factor(theta) = 0.9*exp(j*0.4*pi*(-cos(theta) + 1)) + ...
+    1 + ...
+    0.7*exp(j*0.6*pi*(cos(theta) - 1)) + ...
+    0.5*exp(j*pi*(cos(theta) - 1));
+
+Electric_Trans(theta) = Element_Factor(theta) * Array_Factor(theta);
+
+theta = linspace(-pi, pi, 1000);
+
+E_eval = abs(eval(Electric_Trans));
+E_eval = E_eval ./ max(E_eval);
+E_eval = 20.0 .* log10(E_eval);
+E_eval(E_eval < -20) = -20;
+polarplot(theta, E_eval);
+rlim([-20, 0]);
