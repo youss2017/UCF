cdcf5de62799c509029afee22e6a2aaf03f53231
Youssef Samwel <yo800238@ucf.edu>
2024-04-01 23:43:53 -0400

    4/1/2024

---
+++ Spring 2024/RF Active Circuits/Lab/Lab4/Lab4_wrk/workspace.ads
@@ -0,0 +1,39 @@
+<ADSWorkspace Revision="1" Version="100">
+    <Workspace Name="">
+        <LibraryDefs Name="lib.defs" />
+        <Library Name="ads_standard_layers" />
+        <Library Name="ads_schematic_layers" />
+        <Library Name="empro_standard_layers" />
+        <Library Name="ads_builtin" />
+        <Library Name="ads_standard_layers_ic" />
+        <Library Name="ads_schematic_layers_ic" />
+        <Library Name="ads_schematic_ports_ic" />
+        <Library Name="ads_rflib" />
+        <Library Name="ads_sources" />
+        <Library Name="ads_simulation" />
+        <Library Name="ads_tlines" />
+        <Library Name="ads_bondwires" />
+        <Library Name="ads_datacmps" />
+        <Library Name="ads_behavioral" />
+        <Library Name="ads_textfonts" />
+        <Library Name="ads_common_cmps" />
+        <Library Name="ads_designs" />
+        <Library Name="ads_pelib" />
+        <Library Name="ads_serdes_ref_channel" />
+        <ConfigFile Name="de_sim.cfg" />
+        <ConfigFile Name="hpeesofsim.cfg" />
+        <Preferences Name="layout.prf" />
+        <Preferences Name="schematic.prf" />
+        <Library Name="Lab4_lib" />
+        <Library Name="AT414xx_lib" />
+        <Substrate Name="AT414xx_lib:substrate1.subst" />
+        <Cell Name="AT414xx_lib:AT41400" />
+        <Cell Name="AT414xx_lib:AT41410" />
+        <Cell Name="AT414xx_lib:AT41411" />
+        <Cell Name="AT414xx_lib:AT41435" />
+        <Cell Name="AT414xx_lib:AT41470" />
+        <Cell Name="AT414xx_lib:AT41485" />
+        <Cell Name="AT414xx_lib:AT41486" />
+        <Cell Name="AT414xx_lib:AT414die" />
+    </Workspace>
+</ADSWorkspace>
